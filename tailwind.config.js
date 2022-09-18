d007654d15d895498d314ebe74fccf2edbbd78ab
Nikhilesh Shah <nikhileshshah10@gmail.com>
2022-09-18 13:10:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Licorice': ['Lato', 'sans-serif'] 
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#5d5d5d',
+      },
+    },
+  },
+  plugins: [],
+}
